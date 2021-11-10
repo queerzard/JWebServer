bca3ea4353e0166df1070e7eedec64e3f7e6f59f
Seby_Plays <46566740+SebyPlays@users.noreply.github.com>
2021-11-10 08:27:32 +0000

    Delete ResourceHandler.java
    Obsolete

--- src/com/github/sebyplays/jwebserver/api/ResourceHandler.java
+++
@@ -1,7 +0,0 @@
-package com.github.sebyplays.jwebserver.api;
-
-public class ResourceHandler {
-    //TODO Scan for resources.
-    //TODO if a request is made to a context that isn't statically registered, then the files within the application's
-    // folder are scanned for resources with the same path/name and then sent to add support for images, html & css files etc.
-}
